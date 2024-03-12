b62a5f2c2ceae031cd50f16f8b165825c7f8cbf8
ganpeixin <ganpeixin@mucfc.com>
2024-03-12 17:42:36 +0800

    a

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("CHANGE111")
+    print("CHANGE222")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
